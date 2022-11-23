cd7c87cf67d9e932fc41b1051f155be86171e152
FELIX-MAIN <117909509+FELIX-MAIN@users.noreply.github.com>
2022-11-24 01:23:47 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = (number1, number2) => {
+    console.log(number1 / number2);
+}
+divide(200,100);
+
+const square = (x) => {x * x}
+let x=2 
+console.log(x*x);
+square(2);
+
+const add = (a, b) => a+b;
+let a = 3
+let b = 4
+console.log(a + b);
